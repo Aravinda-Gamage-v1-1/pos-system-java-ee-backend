ddf82cfa92de88cd6884a508e0dd3f5bfa2255e6
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-10-21 23:05:33 +0530

    add packages

---
+++ src/main/java/lk/ijse/possystemjavaeebackend/controller/CustomerController.java
@@ -0,0 +1,8 @@
+package lk.ijse.possystemjavaeebackend.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet (urlPatterns = "/customer")
+public class CustomerController extends HttpServlet {
+}
